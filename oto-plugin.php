6f06a2d9f3f187a8cba2641040450d4609b64d02
MrAnkan <information@d-ank.com>
2016-01-25 13:51:46 +0100

    -

--- oto-plugin.php
+++ oto-plugin.php
@@ -119,7 +119,7 @@ add_action( 'wp_dashboard_setup', 'eter_add_dashboard_widgets' );
 function eter_dashboard_widget_function() {
 
 // Display whatever you want to tell.
-    echo"<p><a href='Gå til OTO-appens inställningar'</a></p>";
+    echo"<p><a href=''>Gå til OTO-appens inställningar</a></p>";
   echo "<p>Det går att dölja innehåll från appen eller webbsidan. Detta görs genom att byta innehållsredigerarens läge från visuell till text, och sedan innefatta innehållen för repsektive plattform inom en lämplig utav dessa: </p><code>&lt;div class='app'&gt; Innehåll &lt/div&gt;  &lt;div class='webb'&gt; Innehåll &lt/div&gt; </code>. <p>'app' visas bara i appen och 'webb' visas bara på webben.</p>";
 }
 
